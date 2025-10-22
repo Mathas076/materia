233544704054e7184f8a79be65bce3a41e4cd41f
mathiaszambrano076 <mathiaszambrano076@gmail.com>
2025-10-22 17:26:14 +0000

    ✨feat(LoginForm): Validacion de correo electronico -Se implemento TextSchema en LoginForm para validar correo electronico -Se agregaron estilos a index

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,10 @@
 import {LoginForm} from "@/components/LoginForm";
 import { Text, View } from "react-native";
+import "@/global.css";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center"
+    <View  className="bg-blue-500 flex-1 justify-center items-center"
     >
       <LoginForm onLoginPress={() => console.log("Login pressed")}></LoginForm>
     </View>
--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,36 +1,58 @@
 import { CustomButton } from '@/components/ui/CustomButton';
 import CustomText from "@/components/ui/CustomText"
 import { TextInput, View } from "react-native";
-
-type LoginFormProps = {
-  onLoginPress: () => void;
-  isDisabled?: boolean;
-}
+import { useState } from 'react';
+import { z } from 'zod';
+import { loginSchema } from '@/lib/schemas/TextSchema';
 
 export function LoginForm({ onLoginPress, isDisabled = true }: LoginFormProps) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState(''); // opcional si quieres validar contraseña después
+  const [error, setError] = useState<string | null>(null);
+
+  const handleLoginPress = () => {
+    // Validar con Zod 
+
+    const result = loginSchema.safeParse({ email });
+    if (!result.success) {
+      // Mostrar error
+      setError(result.error.errors[0].message);
+    } else {
+      setError(null);
+      onLoginPress();
+    }
+  };
+
   return (
     <>
-      <CustomText variant='large' >Inicia sesion!</CustomText> 
+      <CustomText variant='large'>Inicia sesion!</CustomText>
       <CustomText variant='medium'>Ingresa tus datos para acceder a nuestra app!</CustomText>
-      
-      <TextInput 
-        className='bg-white text-black rounded-lg p-3 w-full border border-black' 
-        placeholder='Email/Usuario' 
-      />
-      <TextInput 
-        className='bg-white text-black rounded-lg p-3 w-full border border-black' 
-        placeholder='Contraseña' 
-        secureTextEntry 
-      />
-      
+          
+      <TextInput
+        className='bg-white text-black rounded-lg p-3 w-full border border-black'
+        placeholder='Email'
+        value={email}
+        onChangeText={setEmail}
+        keyboardType='email-address'
+        autoCapitalize='none'
+      />{/*
+      <TextInput
+        className='bg-white text-black rounded-lg p-3 w-full border border-black'
+        placeholder='Contraseña'
+        secureTextEntry
+        value={password}
+        onChangeText={setPassword}
+      />*/}
+
+      {error && <CustomText variant='small'>{error}</CustomText>}
+
       <View className='flex-row gap-2'>
         <CustomButton 
-          onPress={onLoginPress} 
-          variant={isDisabled ? 'link' : 'secondary'}
+        variant='primary'
+        onPress={handleLoginPress}
         >
           Iniciar sesión
         </CustomButton>
-        
       </View>
     </>
   );
