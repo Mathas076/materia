1123e8820046a03a1d102c86cd243049daccb8a5
mathiaszambrano076 <mathiaszambrano076@gmail.com>
2025-11-11 02:12:14 +0000

    💄ui(index): Implementacion de navegacion entre LoginForm y RegisterForm -Se agrego la opcion de moverse entre LoginForm y RegisterForm

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,26 @@
-import {LoginForm} from "@/components/LoginForm";
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { View } from "react-native";
 import "@/global.css";
 
+import { LoginForm } from "@/components/LoginForm";
+import { RegisterForm } from "@/components/RegisterForm";
+
 export default function Index() {
+  const [isRegistering, setIsRegistering] = useState(false);
+
   return (
-    <View  className="bg-blue-500 flex-1 justify-center items-center"
-    >
-      <LoginForm onLoginPress={() => console.log("Login pressed")}></LoginForm>
+    <View className="bg-blue-500 flex-1 justify-center items-center p-4 w-full">
+      {isRegistering ? (
+        <RegisterForm
+          onRegisterPress={() => console.log("Register pressed")}
+          onLoginPress={() => setIsRegistering(false)}  
+        />
+      ) : (
+        <LoginForm
+          onLoginPress={() => console.log("Login pressed")}
+          onRegisterPress={() => setIsRegistering(true)} 
+        />
+      )}
     </View>
   );
 }
